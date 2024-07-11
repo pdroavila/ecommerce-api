cd8ffa1953cd2705e9497793d4ac4776c3e97ee9
Pedro Avila <duidao123gjs@gmail.com>
2024-07-10 22:50:04 -0300

    final settings

--- routes/pedidosRoutes.js
+++ routes/pedidosRoutes.js
@@ -6,9 +6,4 @@ const pedidosController = require('../controllers/pedidosController');
 router.get('/pedidos', pedidosController.listarPedidos);
 router.post('/pedidos', pedidosController.criarPedido);
 
-// router.get('/produtos/storage/:id', produtosController.imagemProduto);
-// router.get('/produtos/:id', produtosController.obterProdutoPorId);
-// router.put('/produtos/:id', produtosController.atualizarProduto);
-// router.delete('/produtos/:id', produtosController.removerProduto);
-
 module.exports = router;
